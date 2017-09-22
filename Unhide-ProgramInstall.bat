ea4e8152115032706472c2d9f84c535260a1eaa6
lscambo13 <32175188+lscambo13@users.noreply.github.com>
2017-09-22 11:41:52 +0530

    Update Unhide-ProgramInstall.bat

--- Unhide-ProgramInstall.bat
+++ Unhide-ProgramInstall.bat
@@ -1,4 +1,4 @@
 @echo off
-copy "file.vbs" "c:\users\public\"
-attrib +h "c:\users\public\file.vbs"
-reg.reg
+copy "Unhide-Action.vbs" "c:\users\public\"
+attrib +h "c:\users\public\Unhide-Action.vbs"
+Unhide-ContextMenu.reg
